5e617ab9a787b0b0454bfc13196a4a92073e4268
dodek08 <32713513+dodek08@users.noreply.github.com>
2018-02-02 17:57:04 +0100

    IT WORKS
    saves data from 11 stops in separate csv files

--- pjs.R
+++ pjs.R
@@ -1,4 +1,26 @@
-url <- paste0('https://mpk.jacekk.net/#!pl77')
+library('rvest')
+library("XML")
+library('stringr')
+
+przystankiWZ <- c('BronwiceMale', 'Biprostal', 'TeatrBagatela',
+                  'DworzecGlowny','RondoCzyzynskie')
+przystankiPP = c('KrowodrzaGorka', 'StaryKleparz','PocztaGlowna','Korona',
+                 'RondoMatecznego','BorekFalecki')
+adresy.WZ <- c('https://mpk.jacekk.net/#!pl135', 'https://mpk.jacekk.net/#!pl84',
+               'https://mpk.jacekk.net/#!pl77', 'https://mpk.jacekk.net/#!pl131',
+               'https://mpk.jacekk.net/#!pl408')
+adresy.PP <- c('https://mpk.jacekk.net/#!pl63', 'https://mpk.jacekk.net/#!pl3032',
+               'https://mpk.jacekk.net/#!pl357', 'https://mpk.jacekk.net/#!pl571',
+               'https://mpk.jacekk.net/#!pl610', 'https://mpk.jacekk.net/#!pl747')
+
+
+przystanki <- c(przystankiWZ, przystankiPP)
+adresy <- c(adresy.WZ, adresy.PP)
+
+for( i in 1:11)
+{
+przystanek <- przystanki[i]
+url <- paste0(adresy[i])
 lines <- readLines("scrape_final.js")
 lines[1] <- paste0("var url ='", url ,"';")
 writeLines(lines, "scrape_final.js")
@@ -7,8 +29,16 @@ writeLines(lines, "scrape_final.js")
 system("phantomjs scrape_final.js")
 
 pg <- read_html("1.html")
-test9 <-html_node(pg,'#times-table')
+node<-html_node(pg,'#times-table')
 
-okurcze <- html_text(test9)
-
-print(okurcze)
+node.xml <- xmlParse(node)
+data <- xmlToDataFrame(node.xml)
+names(data) <- c('Line','Direction','Vehicle','ETA','Late')
+time <- toString(Sys.time())
+title <- paste(time, przystanek, sep = "_")
+title <- gsub(" ", "_", title, fixed = TRUE)
+title <- paste(title,".csv", sep = "")
+title <- paste("saves/",title, sep = "")
+write.csv(data, file = title)
+print(title)
+}
