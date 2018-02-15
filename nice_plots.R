e6c5e46357864cdacd075598d961111d6cdb6736
dodek08 <kasperski.dominik@gmail.com>
2018-02-15 20:15:03 +0100

    new comments

--- nice_plots.R
+++ nice_plots.R
@@ -1,21 +1,26 @@
-# rm(list = ls())
-# library('lattice')
-# library('ggplot2')
-# library(latticeExtra)
-# 
-# df<-load("ALL")
-# ALL$X.2<-NULL
-# ALL$X.1<-NULL
-# ALL$X<-NULL
-# 
-# ALL$Time <- as.POSIXct(ALL$Time, format("%Y-%m-%d %h:%m:%s"))
-# ALL<-ALL[order(ALL$Time),]
-# 
-# # ALL_n<-ALL_n[ALL_n$Late>0,]
-# lines <- unique(ALL$Line)
-# stops <- unique(ALL$StopName)
-# directions <- unique(ALL$Direction)
+rm(list = ls())
+library('lattice')
+library('ggplot2')
+library(latticeExtra)
 
+#clearing data
+df<-load("ALL")
+ALL$X.2<-NULL
+ALL$X.1<-NULL
+ALL$X<-NULL
+
+#dealing with time
+ALL$Time <- as.POSIXct(ALL$Time, format("%Y-%m-%d %h:%m:%s"))
+ALL<-ALL[order(ALL$Time),]
+
+
+#option to choose only delayed ones
+# ALL_n<-ALL_n[ALL_n$Late>0,]
+lines <- unique(ALL$Line)
+stops <- unique(ALL$StopName)
+directions <- unique(ALL$Direction)
+
+#function which clear data set, this is done evert tome so i wrote a function
 order_data_set <- function(ALL = NULL)
 {
   if(is.null(ALL))
@@ -30,7 +35,8 @@ order_data_set <- function(ALL = NULL)
 }
 
 
-
+#function which make time sequence of delays between TMIN and  TMAX with default dt = 3 mins.
+#FROM GIVEN DATA FRAME so be carefull!
 time_and_late_sequence <- function(ALL_n,TMIN,TMAX,dt = 3*60)
 {
   Late <- 0
@@ -54,6 +60,12 @@ time_and_late_sequence <- function(ALL_n,TMIN,TMAX,dt = 3*60)
   
 }
 
+
+#function to plot delays on all stops for line in given direction
+#it also calculates correlation between timesequences of delays on each stop considered
+#you need to pass data frame (preferably ALL) line, direction as from "stops_and_directions.R"
+#date start and stop, defaultly covers 6/02/2018
+#color of correlation plot
 delays_and_correlations <- function(ALL = ALL, line = 50, direction = 'KrowodrzaGorka',
                                     date1 = as.POSIXct("2018-02-06 00:00:00", format("%Y-%m-%d %h:%m:%s")),
                                     date2 = as.POSIXct("2018-02-06 23:59:59", format("%Y-%m-%d %h:%m:%s")),
@@ -61,9 +73,11 @@ delays_and_correlations <- function(ALL = ALL, line = 50, direction = 'Krowodrza
   {
 
 ALL <- order_data_set(ALL)
+#create data frame with data about line in direction in given time interval
 stop_plot <- ALL[ALL$Direction == direction,]
 stop_plot <- stop_plot[stop_plot$Line == line,]
 stop_plot <- stop_plot[stop_plot$Time>date1 & stop_plot$Time<date2 ,]
+#strightforward attempt to automatically find a route of a line
 route <- unique(stop_plot$StopName)
 
 stop_plot$StopName<-as.character(stop_plot$StopName)
@@ -72,6 +86,7 @@ levels(stop_plot$StopName) <- as.character(route)
 route<-as.character(route)
 levels(route)<-route
 
+#plot of delays on all stops of a line in direction
 my.theme <- theEconomist.theme()
 myplot <- xyplot(Late ~ Time | StopName, stop_plot, type = "p", par.settings = my.theme, 
                  xlab = "Time", 
@@ -80,10 +95,12 @@ myplot <- xyplot(Late ~ Time | StopName, stop_plot, type = "p", par.settings = m
                    panel.xyplot(x,y,...);
                    panel.lines(x=date1:date2, y=rep(1,100), col="red") })
 
+#correlations
+# Times <- vector(mode = 'numeric')
+# Late <- vector()
+# StopName <-vector()
 
-Times <- vector(mode = 'numeric')
-Late <- vector()
-StopName <-vector()
+#prepare data frame with time sequences for all stops
 cor_stop<- time_and_late_sequence(ALL_n =  stop_plot[stop_plot$StopName ==  route[1],],
                                   TMIN = date1, TMAX = date2, dt = 10*60)
 cor_stop['StopName']<- as.character(rep(route[1],length(cor_stop[,1])))
@@ -94,16 +111,17 @@ tmp.df <- time_and_late_sequence(ALL_n =  stop_plot[stop_plot$StopName ==  stop,
 tmp.df['StopName']<- rep(stop,length(tmp.df[,1]))
 
 cor_stop<-rbind(cor_stop,tmp.df)
-
-Times <- append(Times,tmp.df[1])
-Late <- append(Late,tmp.df[2])
-StopName <- append(StopName,tmp.df[3])
+# 
+# Times <- append(Times,tmp.df[1])
+# Late <- append(Late,tmp.df[2])
+# StopName <- append(StopName,tmp.df[3])
 }
 
 
-
+#correlation matrix with rows an columns named after stops covered
 cor_mat <- matrix(nrow = length(route), ncol = length(route))
 dimnames(cor_mat)<-list(route,route)
+#calculations
 for(stop in route)
 {
   for(stop2 in route)
@@ -116,9 +134,13 @@ for(stop in route)
 # data_colors <- topo.colors(length(route)+1, alpha = 1)
 # nrows<-ceiling(sqrt(length(route)+1))
 # par(mfrow=c(nrows,nrows))
+
+
+#prepare data to plot correlations
 cors<-2:length(route)
 for (i in 2:length(route))
 {
+
   stop <- route[i-1]
   stop1 <- route[i]
   # print(paste0("Correlation of delays between ",stop," and ", stop1))
@@ -134,6 +156,8 @@ for (i in 2:length(route))
   
 }
 
+#correlation plot intended to be a plot of correlation of delays between stops
+#due to "route" not always works it sometimes does not work too
 matplot(x = c(2:length(route)), y = cors,  type = "l",
         lty = "solid",  lwd = 2,
         xlab = paste0(line," in direction ",direction), ylab = 'Correlation en route',
@@ -148,6 +172,9 @@ for(i in 1:(length(route)-1))
   ypos <- cors[i]
   text(label=format(round(ypos, 2), nsmall = 2), x = xpos, y = ypos+0.05, col = "black")
 }
+
+#prints route to let one know if it is false
 print(route)
+#returns plot of lates for all stops
 return(myplot)
 }
--- ploting_nice_plots.R
+++ ploting_nice_plots.R
@@ -1,24 +1,34 @@
-# rm(list = ls())
-# library('lattice')
-# library('ggplot2')
-# library(latticeExtra)
-# df<-load("ALL")
-# ALL$X.2<-NULL
-# ALL$X.1<-NULL
-# ALL$X<-NULL
-# 
-# ALL$Time <- as.POSIXct(ALL$Time, format("%Y-%m-%d %h:%m:%s"))
-# ALL<-ALL[order(ALL$Time),]
-# 
-# # ALL_n<-ALL_n[ALL_n$Late>0,]
-# lines <- unique(ALL$Line)
-# stops <- unique(ALL$StopName)
-# directions <- unique(ALL$Direction)
+#this file saves almost all plots used in report
 
+rm(list = ls())
+library('lattice')
+library('ggplot2')
+library(latticeExtra)
+
+#prepare data frame
+df<-load("ALL")
+ALL$X.2<-NULL
+ALL$X.1<-NULL
+ALL$X<-NULL
+
+#deal with time
+ALL$Time <- as.POSIXct(ALL$Time, format("%Y-%m-%d %h:%m:%s"))
+ALL<-ALL[order(ALL$Time),]
+
+#choose option which delays to cover
+# ALL_n<-ALL_n[ALL_n$Late>0,]
+lines <- unique(ALL$Line)
+stops <- unique(ALL$StopName)
+directions <- unique(ALL$Direction)
+
+#load functions
 source('nice_plots.R')
+#first plot to check if it works
 t22<-delays_and_correlations(ALL = ALL, line = 2, 
                              direction = 'Salwator', color = data_colors[2])
 trellis.device(device="png", filename=("correlations.png"))
+
+#plot multiple correlations plot at one .png
 data_colors <- topo.colors(4, alpha = 1)
 nrows<-2
 par(mfrow=c(nrows,nrows))
@@ -33,25 +43,27 @@ t4 <- delays_and_correlations(ALL = ALL, line = 18,
                               direction = 'CzerwoneMakiP+R', color = data_colors[4])
 
 dev.off()
-# trellis.device(device="png", filename=paste0("time_series_",t1$ylab,".png"))
-# print(t1)
-# dev.off()
-# 
-# trellis.device(device="png", filename=paste0("time_series_",t2$ylab,".png"))
-# print(t2)
-# dev.off()
-# 
-# trellis.device(device="png", filename=paste0("time_series_",t3$ylab,".png"))
-# print(t3)
-# dev.off()
-# 
-# trellis.device(device="png", filename=paste0("time_series_",t4$ylab,".png"))
-# print(t4)
-# dev.off()
-# 
-# trellis.device(device="png", filename=paste0("time_series_",t22$ylab,".png"))
-# print(t22)
-# dev.off()
+
+#save plots of delays on each stop into files time_series_line_direction.png 
+trellis.device(device="png", filename=paste0("time_series_",t1$ylab,".png"))
+print(t1)
+dev.off()
+
+trellis.device(device="png", filename=paste0("time_series_",t2$ylab,".png"))
+print(t2)
+dev.off()
+
+trellis.device(device="png", filename=paste0("time_series_",t3$ylab,".png"))
+print(t3)
+dev.off()
+
+trellis.device(device="png", filename=paste0("time_series_",t4$ylab,".png"))
+print(t4)
+dev.off()
+
+trellis.device(device="png", filename=paste0("time_series_",t22$ylab,".png"))
+print(t22)
+dev.off()
 
 
 
